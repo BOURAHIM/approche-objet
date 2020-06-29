e3d155145c101198af4dc3dbaa71f0352c285ed2
BOURA Troulé <BOURA Troul@192.168.1.26>
2020-06-29 11:00:53 +0200

    jkh

--- src/entites/Personne.java
+++ src/entites/Personne.java
@@ -1,6 +1,10 @@
 package entites;
 
 public class Personne {
+	public String nom;
+	public String prenom;
+	public String adresse;
+
 	/******* Constructeur *****/
 	public Personne(String nom, String prenom, String adresse) {
 		this.nom = nom;
