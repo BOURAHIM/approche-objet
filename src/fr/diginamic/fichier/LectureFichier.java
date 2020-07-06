70e4b65d5f0f7e457f43db530d62f90f5711e0bd
Iyan BOURAHIM <iyan.bouahim@gmail.com>
2020-07-06 15:37:03 +0200

    M.A.J

--- src/fr/diginamic/fichier/LectureFichier.java
+++ src/fr/diginamic/fichier/LectureFichier.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
+
 
 /**
  * @author BOURA Troulé
@@ -33,7 +35,7 @@ public class LectureFichier {
 	public static void main(String[] args) {
 
 		try {
-			File file = new File("C:\\Diginamic\recensement population 2016.csv");
+			File file = new File("C:\\Diginamic\\recensement population 2016.csv");
 			List<String> lignes = FileUtils.readLines(file, "UTF-8");
 			
 			for(String ligne : lignes) {
@@ -41,6 +43,7 @@ public class LectureFichier {
 			}
 			
 			System.out.println("Nombre de lignes : " + lignes.size());
+		
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
 		}
